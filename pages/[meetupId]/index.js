33fde0d91075b263d07059bc08c330b6112a5fc1
Roman Lesnoy <romanlesnoy@outlook.com>
2022-07-21 16:46:18 +0300

    Add SSG

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ function MeetupDetailsPage() {
     );
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: "m1",
+                },
+            },
+            {
+                params: {
+                    meetupId: "m2",
+                },
+            },
+        ],
+    };
+}
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+    return {
+        props: {
+            meetupData: {
+                id: "m1",
+                image: "httpcls://images.unsplash.com/photo-1536663815808-535e2280d2c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bGlzYm9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+                title: "A first meetup",
+                address: "Some address, Lisbon",
+                descpirtion: "A first meetup",
+            },
+        },
+        revalidate: 10,
+    };
+}
+
 export default MeetupDetailsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -17,8 +18,17 @@ const DUMMY_MEETUPS = [
     },
 ];
 
-function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+    return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS,
+        },
+        revalidate: 10,
+    };
 }
 
 export default HomePage;
